ec6a286fbbf02ee36ffaf21c6530f82118b10ff3
Shally Garg <garg.shally05@gmail.com>
2018-08-25 23:22:47 -0700

    correcting typo

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL){
-  conection = mysql.createConnection(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 }
 else{
   connection = mysql.createConnection({
@@ -16,13 +16,7 @@ else{
 }
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
